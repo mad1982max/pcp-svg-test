b102407f1ba682755ed851276fae9ce4d19b3f61
Maks Bezrodnyi <maks.bezrodnyi@gmail.com>
2020-07-20 14:33:24 +0300

    scaling

--- panotour/levels/index.js
+++ panotour/levels/index.js
@@ -8,6 +8,13 @@ let setFlagObj = {
     set1: false,
     set2: false
 }
+let curPos = {
+    x: 0,
+    y: 0,
+    initPicW: 3000,
+    initPicH: 1850,
+    k: 0
+}
 
 
 window.onload = onloadFn;
@@ -15,7 +22,7 @@ window.onload = onloadFn;
 function defineData4Floor() {
     let paramsString = window.location.search;
     let searchParams = new URLSearchParams(paramsString);
-    level = searchParams.get("level");
+    level = searchParams.get('level');
     floorSrc = `./img/${level}.png`
     coverSrc = new Image();
     coverSrc.src = `./img/${level}_coverGrey.gif`;
@@ -25,14 +32,19 @@ function defineData4Floor() {
 function onloadFn() {
     defineData4Floor();
     
-    header = document.querySelector(".header");
-    footer = document.querySelector(".footer");
+    header = document.querySelector('.header');
+    footer = document.querySelector('.footer');
     container = document.getElementById('container');
-    window.addEventListener("resize", resize);
-    currentRatio = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth/3000;
+    window.addEventListener('resize', resize);    
+    
+    buildSvg();
     resize();
-    buildSvg(buildDefault);
-    //buildDefault();
+    let checkBoxArr = [...document.querySelectorAll('.form-check-input')];
+    checkBoxArr.forEach(box => {
+    setFlagObj[box.id] = box.checked; 
+    checkBoxListener(box.id, box.checked);
+    box.addEventListener('change', (e) => checkBoxListener(e.target.id, e.target.checked))
+    });
     document.body.style.opacity = 1;
 }
 
@@ -41,7 +53,23 @@ function getScreenWidthHeight() {
     wHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     wWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     svgHeight = wHeight - headerFooterHeight;
-    
+
+    if(wWidth > wHeight*curPos.initPicW/curPos.initPicH) {
+        curPos.k = svgHeight/curPos.initPicH;
+        let picW = curPos.initPicW*curPos.k;
+        curPos.x = (wWidth/curPos.k - picW/curPos.k)/2;
+        curPos.y = 0;
+
+    } else {
+        curPos.k = wWidth/curPos.initPicW
+        let picH = curPos.initPicH*curPos.k;
+        curPos.x = 0;
+        curPos.y = 0;
+        curPos.y = (wHeight/curPos.k - picH/curPos.k)/5;;
+        
+    }
+      
+    console.log('curPos.k',curPos.k);   
 }
 
 
@@ -50,142 +78,124 @@ function resize() {
     container.style.height = `${svgHeight}px`;
     container.style.width = `${wWidth}px`;
 
-
     if(mainLayer) {
         mainLayer
-        .attr("transform", `scale(${wWidth/3000})`) 
+        .attr('transform', `scale(${curPos.k}) translate(${curPos.x},${curPos.y})`) 
     }
 
     
 }
 
-function buildDefault() {
-    let checkBoxArr = [...document.querySelectorAll(".form-check-input")];
-    checkBoxArr.forEach(box => {
-    setFlagObj[box.id] = box.checked; 
-    checkBoxListener(box.id, box.checked);
-    box.addEventListener("change", (e) => checkBoxListener(e.target.id, e.target.checked))
-    });
-}
+function buildSvg() {
 
-function buildSvg(fn) {
-    svg = d3.select("#container").append("svg");
+    svg = d3.select('#container').append('svg');
     svg
-        .attr("height", "100%")
-        .attr("width", "100%")
+        .attr('class', 'svgContainer')
+        .attr('height', '100%')
+        .attr('width', '100%')
         
-    mainLayer = svg.append("g");
+    mainLayer = svg.append('g');
     mainLayer
-        .attr("class", "mainLayer")
-        .attr("transform", `scale(${wWidth/3000})`)        
+        .attr('class', 'mainLayer')
+        .attr('transform', `scale(${curPos.k}) translate(${curPos.x},${curPos.y})`)        
 
-    floorLayer = mainLayer.append("g")
+    floorLayer = mainLayer.append('g')
     floorLayer
-        .attr("class", "floorLayer")
+        .attr('class', 'floorLayer')
 
-    let floor = floorLayer.append("image");
-    floor.attr("class", "currentFloor")
-    floor.attr("xlink:href", floorSrc)
+    let floor = floorLayer.append('image');
+    floor.attr('class', 'currentFloor');
+    floor.attr('xlink:href', floorSrc);
+
     const zoom = d3
-    .zoom()
-    .scaleExtent([0.2, 3])
-    .on("zoom", zoomed);
-
-    svg.call(zoom);
-    fn();    
-}   
-
-
+        .zoom()
+        .scaleExtent([0.2, 3])
+        .on('zoom', zoomed);
+    svg.call(zoom);  
+}  
 
 function checkBoxListener(itemToShow, isChecked) {
 
     switch(itemToShow) {
-        case "cover":
+        case 'cover':
             drawCover(itemToShow, isChecked);
             break;
-        case "set1":
-        case "set2":
+        case 'set1':
+        case 'set2':
             drawSet(itemToShow, isChecked);
             break;
         default:
-            console.log("--nothing to draw");            
+            console.log('--nothing to draw');            
     }
 }
 
 function drawSet(itemToShow, isChecked) {
     if (isChecked) {
-        set = mainLayer.append("g")
-        set.attr("class", "set")
-        //.attr("transform", `scale(${Math.min(wHeight/1850/1.1, wWidth/3000)})`)
-        .selectAll("g")        
-        .data(set1)
-        .join("g")
-        .attr("pointer-events", "visible")
-        .attr("cursor", "pointer")
-        .attr("id", d => d.id)
-        .append("circle")
-            .attr("fill", "green")
-            .attr("cx", d => {
-                return d.x
-            })
-            .attr("cy", d => {
-                return (d.y + 165)
-            })
-            .attr("r", 30)
-            .on("click", clickedOnPin)        
+        set = mainLayer.append('g')
+        set.attr('class', 'set')
+            .selectAll('g')        
+            .data(set1)
+            .join('g')
+            .attr('pointer-events', 'visible')
+            .attr('cursor', 'pointer')
+            .attr('id', d => d.id)
+            .append('circle')
+                .attr('fill', '#ff0066')
+                .attr('cx', d => {
+                    return d.x
+                })
+                .attr('cy', d => {
+                    return (d.y + 165)
+                })
+                .attr('r', 30)
+                .on('click', clickedOnPin)        
 
         set
-        .selectAll("g")        
-        .data(set1)
-        .join("g")
-        .append("text")
-        .attr("x", d => {
-            return d.x
-        })
-        .attr("y", d => {
-            return (d.y + 165)
-        })
-        .attr("text-anchor", "middle")
-        .attr("font-size", 30)
-        .attr("fill", "white")
-        .attr("font-family", "sans-serif")
-        .attr("dy", "10")
-        .attr("pointer-events", "none")
-        .text(d => d.id);        
-        
+            .selectAll('g')        
+            .data(set1)
+            .join('g')
+            .append('text')
+            .attr('x', d => {
+                return d.x
+            })
+            .attr('y', d => {
+                return (d.y + 165)
+            })
+            .attr('text-anchor', 'middle')
+            .attr('font-size', 30)
+            .attr('fill', 'white')
+            .attr('font-family', 'sans-serif')
+            .attr('dy', '10')
+            .attr('pointer-events', 'none')
+            .text(d => d.id);          
     } else {
-        if(set) svg.select(".set").remove();
+        if(set) svg.select('.set').remove();
     }
 }
 
 function clickedOnPin(d) {
-    console.log("clicked pin:", d);    
+    console.log('clicked pin:', d);    
 };
 
 
 function drawCover(itemToShow, isChecked) {
-    console.log("cover to build:", itemToShow, isChecked);
-
+    console.log('cover to build:', itemToShow, isChecked);
     if (isChecked) {
-        cover = floorLayer.append("image");
-        cover.attr("xlink:href", coverSrc.src)
-        //.attr("transform", `scale(${Math.min(wHeight/1850/1.1, wWidth/3000)})`)
+        cover = floorLayer.append('image');
+        cover.attr('xlink:href', coverSrc.src)
     } else {
         if(cover) cover.remove();
     }
 }
 
-
-
 function zoomed() {
-    
     const {transform} = d3.event;  
-    console.log("d3.event", d3.event, transform);
     let {k,x,y} = transform;
-    var transform2 = d3Transform()
+    let transform2 = d3Transform() 
         .translate([x, y])
-        .scale(k*wWidth/3000);
-    mainLayer.attr("transform", transform2);    
+        .scale(k*curPos.k)
+        .translate([curPos.x, curPos.y])
+    mainLayer.attr('transform', transform2);    
 }
 
 
--- panotour/mainScheme/js/index.js
+++ panotour/mainScheme/js/index.js
@@ -1,22 +1,28 @@
-let svg, floorRect, headerFooterHeight, obj, img;
+let svg, floorRect, headerFooterHeight, img, w, h;
 let header = document.querySelector('.header');
 let footer = document.querySelector('.footer');
-let myEfficientFn = debounce(resizeFn, 250);
-obj = document.getElementById('svg');
+let resizeDebounce = debounce(resizeFn, 250);
+let obj = document.getElementById('svg');
 
 window.onload = function() {
     resizeFn();
     window.addEventListener('resize', () => {
         document.body.style.opacity = 0;
-        myEfficientFn();
+        resizeDebounce();
     });
     obj.onload = function () {
         let svgDocument = obj.contentDocument;
         svg = svgDocument;
         img = svgDocument.querySelector('.floor');
-        img.setAttribute("height", "100%")
-        img.setAttribute("width", "100%")
-        floorRect = [...svgDocument.querySelectorAll(".block")];
+        img.setAttribute('width', '100%')
+        if(w>600) {
+            let g18 = svg.querySelector('#g18');
+            let {height: imgHeight} = img.getBoundingClientRect();
+            g18.setAttribute('transform', `translate(0, -${(h - imgHeight)/3})`)
+        } else {
+
+        }
+        floorRect = [...svgDocument.querySelectorAll('.block')];
         floorRect.forEach(singleBlock => {
             singleBlock.addEventListener('click', clickFloor)
             singleBlock.addEventListener('mouseenter', mouseOverFloor)
@@ -44,38 +50,34 @@ function debounce(func, wait, immediate) {
 function resizeFn() {
     document.body.style.opacity = 0;
     headerFooterHeight = header.offsetHeight + footer.offsetHeight;
-
-    let h = window.innerHeight || document.documentElement.clientHeight ||
+    h = window.innerHeight || document.documentElement.clientHeight ||
         document.body.clientHeight;
-    let w = window.innerWidth || document.documentElement.clientWidth ||
+    w = window.innerWidth || document.documentElement.clientWidth ||
         document.body.clientWidth;
-    if (w < 600) {
-        obj.setAttribute('data', "./img/all_mini.svg")
+    if (w < 600 && w < h) {
+        obj.setAttribute('data', './img/all_mini.svg')
     } else {
-        obj.setAttribute('data', "./img/allFloorsScheme_.svg")
+        obj.setAttribute('data', './img/allFloorsScheme.svg')
     }   
-
-    // let hwRatio = h / w;
-    // let multer = hwRatio > 1.8 ? 0.7 : hwRatio <= 1 ? 1.2 : 0.8;
     let hSVG = h - headerFooterHeight;
-    obj.style.height = hSVG + "px";
+    obj.style.height = hSVG + 'px';
     document.body.style.opacity = 1;
 }
 
 function mouseLeave() {
-    this.style.fill = "none";
-    this.style.stroke = "none"
+    this.style.fill = 'none';
+    this.style.stroke = 'none'
 }
 
 function mouseOverFloor() {
-    this.style.fill = "rgba(0,0,0,0.2)";
-    this.style.stroke = "#FFF773"
+    this.style.fill = 'rgba(0,0,0,0.2)';
+    this.style.stroke = '#FFF773'
     this.style.strokeWidth = 8;
-    this.style.cursor = "pointer";
+    this.style.cursor = 'pointer';
 }
 
 function clickBack() {
-    setTimeout(function(){document.location.href = `../../index.html?v=${01}`},250);
+    setTimeout(function(){document.location.href = `../../index.html`},250);
 }
 
 function clickFloor(e) {
